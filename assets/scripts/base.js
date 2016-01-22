c33652d08271367f43e75f09870d9bf0eee176ab
Kevin Andrews <kandrews@industrialagency.ca>
2016-01-22 16:27:31 -0500

    reworked file system, added breakpoint file, removed a11y and IE files, updated boilerplate styles

--- assets/scripts/base.js
+++ assets/scripts/base.js
@@ -1,21 +1,20 @@
+/* Chrome fix for focusing jump link elements */
 window.addEventListener("hashchange", function(event) {
-
-    var element = document.getElementById(location.hash.substring(1));
-
-    if (element) {
-
-        if (!/^(?:a|select|input|button|textarea)$/i.test(element.tagName)) {
-            element.tabIndex = -1;
-        }
-
-        element.focus();
+  var element = document.getElementById(location.hash.substring(1));
+  if (element) {
+    if (!/^(?:a|select|input|button|textarea)$/i.test(element.tagName)) {
+      element.tabIndex = -1;
     }
-
-}, false); 
-
+    element.focus();
+  }
+}, false);
 
 (function($){
+  /* Activate scroll reveal animation library */
+
   window.sr = new scrollReveal();
+
+  /* Generic dropdown toggle control */
 
   $('.drawer-toggle').click(function(event){
     event.preventDefault();
@@ -32,5 +31,21 @@ window.addEventListener("hashchange", function(event) {
       }
     }
   });
+
+  /* Smooth scrolling if requested */
+
+  // $('a[href*=#]:not([href=#])').click(function() {
+  //   if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+  //     var target = $(this.hash);
+  //     target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+  //     if (target.length) {
+  //       target.focus();
+  //       $('html,body').animate({
+  //         scrollTop: target.offset().top
+  //       }, 1000);
+  //       return false;
+  //     }
+  //   }
+  // });
 
 })(jQuery);
