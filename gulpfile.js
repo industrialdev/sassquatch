d9674ad0c1177523769cbcf131e148dd96b149fb
Kevin Andrews <kandrews@industrialagency.ca>
2016-11-28 09:13:03 -0500

    added image optimization

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 var plumber = require('gulp-plumber');
 var notify = require('gulp-notify');
+var imagemin = require('gulp-imagemin');
 
 gulp.task('default', ['webserver', 'sass', 'watch']);
 
@@ -55,6 +56,12 @@ gulp.task('scripts', function() {
     .pipe(rename('sswatch.min.js'))
     .pipe(uglify())
     .pipe(gulp.dest('./assets/scripts/min'));
+});
+
+gulp.task('images', function(){
+  gulp.src(['./assets/images/*', '!./assets/images/min'])
+    .pipe(imagemin())
+    .pipe(gulp.dest('./assets/images/min'))
 });
 
 // error notifications using GULP Notify
