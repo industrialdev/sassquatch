85b8cb1b1c85a26c954519fb594d6fe9e16cf48a
Kevin Andrews <kandrews@industrialagency.ca>
2018-05-01 13:58:37 -0400

    import styles into react

--- sassquatch-react/client/app/index.js
+++ sassquatch-react/client/app/index.js
@@ -10,12 +10,11 @@ import {
 
 import App from './components/App/App';
 import NotFound from './components/App/NotFound';
-
 import Home from './components/Home/Home';
-
 import HelloWorld from './components/HelloWorld/HelloWorld';
 
-import './styles/styles.scss';
+// Load stylesheet from theme folder
+import '../../../sassquatch-theme/src/assets/styles/sassquatch.scss';
 
 render((
   <Router>
