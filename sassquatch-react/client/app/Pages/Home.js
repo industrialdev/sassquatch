4d6e027912f91b9a2e5e19bf62a82891df293434
Kevin Andrews <kandrews@industrialagency.ca>
2018-05-10 16:54:43 -0400

    alert component

--- sassquatch-react/client/app/Pages/Home.js
+++ sassquatch-react/client/app/Pages/Home.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import Button from '../Components/Button';
 import Icon from '../Components/Icon';
+import Alert from '../Components/Alert';
 
 class Home extends Component {
   buttonHandleClick(event) {
@@ -12,23 +13,33 @@ class Home extends Component {
     return (
       <>
         <p>
-          <Button btnSize="large" onClick={this.buttonHandleClick.bind(this)}>Large Button</Button>
-          <Button onClick={this.buttonHandleClick.bind(this)}>Default Button</Button>
-          <Button btnSize="small" onClick={this.buttonHandleClick.bind(this)}>Small Button</Button>
+          <Button btnSize="large">Large Button</Button>
+          <Button>Default Button</Button>
+          <Button btnSize="small">Small Button</Button>
         </p>
         <p>
           <Button path="/counter">Internally Linked button</Button>
           <Button link="https://industrialagency.ca">Externally Linked button</Button>
-          <Button btnStyle="primary" onClick={this.buttonHandleClick.bind(this)}>Primary Button</Button>
-          <Button btnStyle="secondary" onClick={this.buttonHandleClick.bind(this)}>Secondary Button</Button>
+        </p>
+        <p>
+          <Button onClick={this.buttonHandleClick.bind(this)}>Default Button with Click Event</Button>
+          <Button btnStyle="primary">Primary Button</Button>
+          <Button btnStyle="secondary">Secondary Button</Button>
+          <Button disabled>Disabled Button</Button>
+        </p>
+        <p>
           <Button iconBefore="info-circle">Button with icon before</Button>
           <Button iconAfter="info-circle" iconStyle="solid">Button with icon after</Button>
-          <Button iconBefore="info-circle" iconAfter="info-circle" Style="light" disabled>Button with icon before and after</Button>
+          <Button iconBefore="info-circle" iconAfter="info-circle" Style="light">Button with icon before and after</Button>
         </p>
         <p>
           <Icon iconName="info-circle" /> This icon is hidden from screen readers<br/>
           <Icon iconName="info-circle" label="this is a test" /> This icon has a label which is read by screen readers
         </p>
+        <Alert iconName="info-circle" iconStyle="solid">This is a default alert</Alert>
+        <Alert iconName="exclamation-triangle" iconStyle="solid" alertStyle="warning">This is a warning alert</Alert>
+        <Alert iconName="exclamation-circle" iconStyle="solid" alertStyle="danger">This is a danger alert</Alert>
+        <Alert iconName="check-circle" iconStyle="solid" alertStyle="success">This is a success alert</Alert>
       </>
     );
   }
