9f16a02f6bd95cffa28f77d63b85c482a1b3639c
Kevin Andrews <kandrews@industrialagency.ca>
2018-06-04 13:33:59 -0400

    supported for nested tabs

--- packages/react/assets/scripts/sassquatch-tabs.js
+++ packages/react/assets/scripts/sassquatch-tabs.js
@@ -2,9 +2,9 @@
   $('.tabs__tab-control').click(function(e){
     e.preventDefault();
     var target = $(this).attr('href');
-    $('.tabs__tab-control').removeClass('active').attr('aria-selected', 'false');
+    $(this).closest('.tabs').find('> .tabs__list > .tabs__tab-control').removeClass('active').attr('aria-selected', 'false');
     $(this).addClass('active').attr('aria-selected', 'true');
-    $('.tabs__content').removeClass('active');
+    $(this).closest('.tabs').find('> .tabs__content-container > .tabs__content').removeClass('active');
     $(target).addClass('active');
   });
 })(jQuery);
